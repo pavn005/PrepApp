4e0dc55da12d553af35d9ccd9aec7ea859ff9352
pavn005 <cpavanreddy2022@gmail.com>
2025-08-26 23:08:33 +0530

    routing in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast"
+
+import Login from "./pages/Auth/Login"
+import SignUp from "./pages/Auth/SignUp"
+import LandingPage from "./pages/LandingPage"
+import DashBoard from "./pages/Home/DashBoard"
+import InterviewPrep from "./pages/Interviewprep/Interviewprep"
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />  
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/dashboard' element={<DashBoard />} />
+          <Route path='/interview-prep/:sessionId' element={<InterviewPrep />} />
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Interviewprep/Interviewprep.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Interviewprep = () => {
+  return (
+    <div>Interviewprep</div>
+  )
+}
+
+export default Interviewprep
