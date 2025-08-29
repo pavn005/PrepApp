2af1bc46ef60500380a803baebed3532b085132f
pavn005 <cpavanreddy2022@gmail.com>
2025-08-30 02:09:38 +0530

    Authentication API's

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,13 +1,13 @@
-import { Schema, model } from "mongoose";
+const mongoose = require("mongoose");
 
-const userSchema = new Schema(
-    {
-        name: {type: String,required: true},
-        email: {type: String,required: true,unique: true},
-        password: {type: String,required: true},
-        profileImageUrl: {type: String,default: null},
-    },
-    {timestamps: true}
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profileImageUrl: { type: String, default: null },
+  },
+  { timestamps: true }
 );
 
-export default model("User",userSchema);
+module.exports = mongoose.model("User", UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 // Middleware to handle cors
@@ -21,14 +23,14 @@ app.use(express.json());
 
 //Routes
 app.use("/api/auth",authRoutes);
-app.use("/api/session",sessionRoutes);
-app.use("/api/questions",questionRoutes);
+// app.use("/api/session",sessionRoutes);
+// app.use("/api/questions",questionRoutes);
 
-app.use("/api/ai/generate-questions", protect,generateInterviewQuestions);
-app.use("/api/ai/generate-explanation", protect,generateConceptExplanation);
+// app.use("/api/ai/generate-questions", protect,generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect,generateConceptExplanation);
 
-//server uploads folder
-app.use("/uploads",express.static(path.join(__dirname,"uploads"),{}));
+// //server uploads folder
+// app.use("/uploads",express.static(path.join(__dirname,"uploads"),{}));
 
 const PORT = process.env.PORT || 5000;
 
